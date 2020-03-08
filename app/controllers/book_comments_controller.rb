b58902cb10d584cc40b0401e6d5e094d2d53fd06
stat-ki <per_ik@outlook.com>
2020-03-08 08:59:26 +0000

    #5 Modify book_comments controller & views

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,12 +3,12 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
     @book_new = Book.new
     @book_comment = @book.book_comments.new(book_comment_params)
     @book_comment.user_id = current_user.id
     if @book_comment.save
       flash[:success] = "Comment was successfully created."
-      redirect_to book_path(@book)
     else
       @book_comments = BookComment.where(book_id: @book.id)
       render '/books/show'
@@ -17,12 +17,12 @@ class BookCommentsController < ApplicationController
 
   def destroy
     @book_comment = BookComment.find(params[:book_id])
+    @book = Book.find(@book_comment.book_id)
+    @book_comments = @book.book_comments
     if @book_comment.user != current_user
       redirect_to request.referer
     end
     @book_comment.destroy
-    redirect_to request.referer
-
   end
 
   private
